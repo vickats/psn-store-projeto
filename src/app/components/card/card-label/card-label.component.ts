165e881e5cf1db02eab4b4c13d8f5f56c88c19a2
vickats <victoriaktsuha@outlook.com>
2023-09-11 15:53:00 -0300

    update psn store project

---
+++ src/app/components/card/card-label/card-label.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-label',
+  templateUrl: './card-label.component.html',
+  styleUrls: ['./card-label.component.scss']
+})
+export class CardLabelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
